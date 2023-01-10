8f2aae45090b1674bebb03047486374001172cad
Charles Lee <cwlee1124@gmail.com>
2023-01-10 21:20:35 +0900

    :rocket: v0.1

--- extractor/wwr.py
+++ extractor/wwr.py
@@ -2,7 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 
 
-def extract_jobs(keyword):
+def extract_wwr_jobs(keyword):
   base_url = "https://weworkremotely.com/remote-jobs/search?term="
 
   response = get(f"{base_url}{keyword}")
@@ -23,9 +23,9 @@ def extract_jobs(keyword):
         title = anchor.find('span', class_='title')
         job_data = {
           'link': f"https://weworkremotely.com{link}",
-          'company': company.string,
-          'region': region.string,
-          'position': title.stirng
+          'company': company.string.replace(","," "),
+          'location': region.string.replace(","," "),
+          'position': title.string.replace(","," "),
         }
         results.append(job_data)
     return results
